78aee19344f29c5b0c271fc89a1b1a4628239532
Dominique Hazael-Massieux <dom@w3.org>
2014-11-04 00:20:17 +0100

    test with multiple non-w3.org resources

--- test/mocha.js
+++ test/mocha.js
@@ -8,6 +8,10 @@ var tests = [
      "input": "one-nonw3org.html",
      "output": ["http://example.org/"]
     },
+    {"desc": "find two non-w3.org resource",
+     "input": "two-nonw3org.html",
+     "output": ["http://example.org/", "http://example.com/"]
+    },
     {"desc": "find one non-w3.org resource loaded via XHR",
      "input": "xhr.html",
      "output": ["http://example.org/"]
